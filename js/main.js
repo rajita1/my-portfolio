6b38a99b5ec08b115210e059424cb88ac321bf1c
rajita1 <rajita_y@hotmail.com>
2018-11-13 14:41:22 -0800

    e are my changes Please enter the commit message for your changes. Lines starting

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work.js';
 
-console.log("Loaded react-dom")
+const myWork = [
+  {
+    'title': "Tickled Pink",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "Tickled Pink",
+      'src': "images/tickled.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Carried Away",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "Carried Away",
+      'src': "images/carried.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "And God Created Woman",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "And God Created Woman",
+      'src': "images/godwoman.jpg",
+      'comment': "And God Created Woman"
+    }
+  }
+];
+
+
+ ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
