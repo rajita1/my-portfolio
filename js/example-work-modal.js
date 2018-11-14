af5539e3761994d96f1c769509a3d48c6711dcd0
rajita1 <rajita_y@hotmail.com>
2018-11-13 19:15:16 -0800

    Fixed modal window

--- js/example-work-modal.js
+++ js/example-work-modal.js
@@ -12,16 +12,13 @@ class ExampleWorkModal extends React.Component {
           <i className="fa fa-window-close-o"></i>
         </span>
         <img alt={example.image.desc}
-             className="modal__image"
+             className="section__exampleImage"
              src={example.image.src}/>
         <div className="color--cloud modal__text">
           <h2 className="modal__title">
             {example.title}
           </h2>
-          <a className="color--fuchsia modal__link"
-             href={example.href}>
-             Click me!
-          </a>
+
           <p className="modal__description">
             {example.desc}
           </p>
--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ import ExampleWork from './example-work.js';
 const myWork = [
   {
     'title': "Tickled Pink",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    'desc': "My signature creation and current masterpiece! Imagine a 60-inch long Sterling Silver chain that starts out as an earring and wraps around the neck in layers and layers of about 30 luxurious fuschia feathers and about 50 pink Swarovski bicones. The end of the necklace has an earring hook (42mm curved tube hoop with 1.5mm ball) that can hook to the other year or on to the necklace. For the bold sassy woman who’s not afraid to make a statement.",
     'image': {
       'desc': "Tickled Pink",
       'src': "images/tickled.jpg",
@@ -15,8 +15,8 @@ const myWork = [
   },
   {
     'title': "Carried Away",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    'desc': "This necklace reminds me of lightness, like a warm summer breeze, or falling in love and that first kiss. As the feather caresses your collarbone, you'll want to keep feeling the softness as a cozy emotional retreat from the cares of the world.",
     'image': {
       'desc': "Carried Away",
       'src': "images/carried.jpg",
@@ -25,8 +25,8 @@ const myWork = [
   },
   {
     'title': "And God Created Woman",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    
+    'desc': "16 inch Sterling Silver Necklace with a Focal Orchid piece attached to a 5 inch Sterling Silver chain dangle. The whole piece is adorned with femininely dainty pink Swarovski bicone crystals. Makes n uniquely gorgeous bridal piece or a great companion for that those black chiffon cocktail dresses.",
     'image': {
       'desc': "And God Created Woman",
       'src': "images/godwoman.jpg",
